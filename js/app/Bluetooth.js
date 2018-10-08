019e889e874263ccdd541f92811e43f79b7a2de2
xiaohaobin <499970382@qq.com>
2018-10-08 13:23:35 +0800

    更新插件

--- js/app/Bluetooth.js
+++ js/app/Bluetooth.js
@@ -5,8 +5,11 @@ window.Bluetooth = function() {
 /**  * 初始化  */
 Bluetooth.prototype.init = function() {		
 	this.main = plus.android.runtimeMainActivity();	
-	this.BluetoothAdapter = plus.android.importClass("android.bluetooth.BluetoothAdapter");
-	this.BAdapter = new this.BluetoothAdapter.getDefaultAdapter();
+	this.BluetoothAdapter = plus.android.importClass("android.bluetooth.BluetoothAdapter");//蓝牙适配器BluetoothAdapter对象
+	this.BAdapter = new this.BluetoothAdapter.getDefaultAdapter();//蓝牙适配器BluetoothAdapter管理对象
+	
+	this.BluetoothDevice = plus.android.importClass("android.bluetooth.BluetoothDevice");//BluetoothDevice用于指代某个蓝牙设备，通常表示对方设备
+	this.BDevice = new this.BluetoothDevice();
 }
 /**  
  * * 判断蓝牙状态，并且弹出提示打开
@@ -44,4 +47,85 @@ Bluetooth.prototype.eventState = function() {
 	} else {
 		this.BAdapter.disable();
 	}
+}
+
+//打开蓝牙功能
+Bluetooth.prototype.open = function(){
+	this.BAdapter.enable();
+}
+//关闭蓝牙功能
+Bluetooth.prototype.close = function(){
+	this.BAdapter.disable();
+}
+//开始搜索周围的蓝牙设备； 
+Bluetooth.prototype.startDiscovery = function(){
+	this.BAdapter.startDiscovery();
+	if(this.BAdapter.isDiscovering()){
+		plus.nativeUI.toast("正在搜索周围的蓝牙设备...");
+	}else{
+		plus.nativeUI.alert("蓝牙搜索功能调用失败...");
+	}
+}
+//取消搜索操作； 
+Bluetooth.prototype.cancelDiscovery = function(){	
+	this.BAdapter.cancelDiscovery();
+	if(!this.BAdapter.isDiscovering()){
+		plus.nativeUI.toast("取消搜索周围的蓝牙设备了");
+	}else{
+		plus.nativeUI.alert("取消搜索失败...");
+	}
+}
+/**
+ * 判断当前是否正在搜索设备
+ * @return {Boolean}
+ * */
+Bluetooth.prototype.isDiscovering = function(){
+	return this.BAdapter.isDiscovering();
+}
+/**
+ * 获取已绑定的设备列表
+ * @return {Array} 
+ * */
+Bluetooth.prototype.getBondedDevices = function(){
+	var sList = this.BAdapter.getBondedDevices() + "";	
+	if(sList.indexOf(",") == -1){		
+		var arr = [];
+		var oneList = sList.replace("[","").replace("]","");
+		arr.push(oneList)
+		return arr;
+	}
+	else{
+		var moreList = sList.replace(" ","").replace("[","").replace("]","");
+		console.log(moreList.length);
+		var a = moreList.split(",");
+		return a;
+	}
+}
+/**
+ * 设置本机蓝牙名称
+ * @param {String} name 蓝牙名称
+ * */
+Bluetooth.prototype.setName = function(name){
+	this.BAdapter.setName(name);
+}
+
+/**
+ * 根据蓝牙地址获取远程的蓝牙设备
+ * @param {String} address 蓝牙地址，如:e4:21:ad:12:cd:1a
+ * @return {String}
+ * */
+Bluetooth.prototype.getRemoteDevice = function(address){
+	return this.BAdapter.getRemoteDevice(address);
+}
+
+/**
+ * 获取本地蓝牙适配器的状态； 
+ * @return {Number}
+ * */
+Bluetooth.prototype.getState = function(){
+	return this.BAdapter.getState();
+}
+
+Bluetooth.prototype.BDevice_getName = function(){
+	return plus.android.invoke(this.BluetoothDevice,"show");
 }
--- js/app/html5_plus.js
+++ js/app/html5_plus.js
@@ -5,7 +5,10 @@ try {
 		 * 返回手机信息
 		 * @param {String} param 手机设备的各个信息，
 		 * param =》 "imei" 获取手机imei（设备的国际移动设备身份码）
-		 * .....
+		 * param =》 "imsi" 获取手机imsi（设备的国际移动用户识别码）
+		 * param =》 "model" 获取设备型号
+		 * param =》 "vendor" 获取设备的生产厂商
+		 * param =》 "uuid" 获取设备的唯一标识
 		 */
 		getDeviceInfo: function(param) {
 			switch(param) {
@@ -14,9 +17,6 @@ try {
 					break;
 				case "imsi": //获取手机imsi（设备的国际移动用户识别码）
 					return plus.device.imsi;
-					break;
-				case "name": //返回手机操作系统版本
-					return plus.os.name;
 					break;
 				case "model": //获取设备型号
 					return plus.device.model;
@@ -31,6 +31,33 @@ try {
 					break;
 			}
 		},
+		/**
+		 * 获取系统信息
+		 * @param {String} param 系统各个信息
+		 * param =》 "language" 获取系统语言信息
+		 * param =》 "name" 获取系统名称信息
+		 * param =》 "version" 获取系统版本信息
+		 * param =》 "vendor" 获取系统的供应商信息
+		 * */
+		getOsInfo: function(param) {
+			switch(param) {
+				case "language":
+					return plus.os.language;
+					break;
+				case "name":
+					return plus.os.name;
+					break;
+				case "version":
+					return plus.os.version;
+					break;
+				case "vendor":
+					return plus.os.vendor;
+					break;
+				default:
+					break;
+			}
+		},
+
 		/**
 		 * 获取当前网络类型
 		 * @return {String}
@@ -47,26 +74,26 @@ try {
 			return types[plus.networkinfo.getCurrentType()];
 		},
 		//获取当前的加速度信息
-		getCurrentAcceleration:function(){
+		getCurrentAcceleration: function() {
 			plus.accelerometer.getCurrentAcceleration(function(a) {
 				alert('X轴：' + a.xAxis + '\nY轴：' + a.yAxis + '\nZ轴：' + a.zAxis);
 			}, function(e) {
 				alert('获取失败:' + e.message);
 			});
-//			return _acc;
+			//			return _acc;
 		},
 		//扫描二维码
-		scan:null,
+		scan: null,
 		/**
 		 * 开始扫描
 		 * @param {String} id 要显示二维码控件的DOM节点的id值
 		 * @param {Function} fn 扫描成功的回调函数，回调参数 type, result, file
 		 * */
-		startScan:function(id,fn){
+		startScan: function(id, fn) {
 			mobile_function.scan = new plus.barcode.Barcode(id);
-			mobile_function.scan.onmarked = function(type, result, file){
+			mobile_function.scan.onmarked = function(type, result, file) {
 				switch(type) {
-						case plus.barcode.QR:
+					case plus.barcode.QR:
 						type = 'QR';
 						break;
 					case plus.barcode.EAN13:
@@ -81,19 +108,421 @@ try {
 				}
 				result = result.replace(/\n/g, '');
 				fn(type, result, file);
-//				alert(type + "," + result + "," + file);
+				//				alert(type + "," + result + "," + file);
 			};
-			mobile_function.scan.start();    
+			mobile_function.scan.start();
 		},
 		//开启闪光灯
-		ScanFlashlight:function(){
-			if(mobile_function.scan == null){
+		ScanFlashlight: function() {
+			if(mobile_function.scan == null) {
 				plus.nativeUI.alert("请先打开扫描二维码控件");
-			}else{
-//				alert(123);
+			} else {
 				mobile_function.scan.setFlash(true);
 			}
-		}
+		},
+		//取消扫描
+		ScanCancel: function() {
+			if(mobile_function.scan == null) {
+				plus.nativeUI.alert("请先打开扫描二维码控件");
+			} else {
+				mobile_function.scan.cancel();
+				mobile_function.scan = null;
+				plus.nativeUI.toast("取消扫描了");
+			}
+
+		},
+		//调用蜂蜜
+		beep: function() {
+			switch(plus.os.name) {
+				case "iOS":
+					if(plus.device.model.indexOf("iPhone") >= 0) {
+						plus.device.beep();
+						plus.nativeUI.toast("设备蜂鸣中...");
+					} else {
+						plus.nativeUI.toast("此设备不支持蜂鸣...");
+					}
+					break;
+				default:
+					plus.device.beep();
+					plus.nativeUI.toast("设备蜂鸣中...");
+					break;
+			}
+		},
+		//调用手机震动
+		vibrate: function() {
+			switch(plus.os.name) {
+				case "iOS":
+					if(plus.device.model.indexOf("iPhone") >= 0) {
+						plus.device.vibrate();
+						plus.nativeUI.toast("设备振动中...");
+					} else {
+						plus.nativeUI.toast("此设备不支持振动");
+					}
+					break;
+				default:
+					plus.device.vibrate();
+					plus.nativeUI.toast("设备振动中...");
+					break;
+			}
+		},
+		/**
+		 * 打电话
+		 * @param {Number} phoneNum 指定号码
+		 * */
+		dial: function(phoneNum) {
+			plus.device.dial(phoneNum, false);
+		},
+		/**
+		 * 创建通讯录联系人
+		 * @param {String} sName 要创建的联系人的名字
+		 * @param {String} sPhoneNum 要创建的联系人电话号码
+		 * @param {String} sType 要创建的联系人号码类型 【mobile（手机）,home（住宅）,other（其他）,custom（定制）】
+		 * */
+		contact_create: function(sName, sPhoneNum, sType) {
+			// 扩展API加载完毕，现在可以正常调用扩展API
+			plus.contacts.getAddressBook(plus.contacts.ADDRESSBOOK_PHONE, function(addressbook) {
+				// 向通讯录中添加联系人
+				var contact = addressbook.create();
+				contact.name = {
+					givenName: sName
+				};
+				contact.phoneNumbers = [{
+					type: sType,
+					value: sPhoneNum,
+					preferred: true
+				}];
+				contact.save();
+				plus.nativeUI.toast("联系人" + sName + "创建成功！");
+			}, function(e) {
+				plus.nativeUI.alert("获取通讯录管理失败: " + e.message);
+			});
+		},
+		/**
+		 * 根据电话号码删除联系人
+		 * @param {String} phoneNum 电话号码
+		 * */
+		contact_remove: function(phoneNum) {
+			plus.contacts.getAddressBook(plus.contacts.ADDRESSBOOK_PHONE, function(addressbook) {
+				// 可通过addressbook进行通讯录操作
+				var contact = addressbook.find(
+					null,
+					function(contacts) {
+						if(contacts.length > 0) {
+							var dc = contacts[0];
+							dc.remove(function() {
+								plus.nativeUI.toast("删除 " + dc.displayName + " 成功!");
+							});
+						}
+					},
+					function(e) {
+						plus.nativeUI.toast("查询联系人失败: " + e.message);
+					}, {
+						filter: [{
+							logic: "or",
+							field: "phoneNumbers",
+							value: phoneNum
+						}],
+						multi: false
+					}
+				);
+			});
+		},
+		/**
+		 * 设置应用是否保持唤醒（屏幕常亮）状态
+		 * @param {Boolean} bLock 是否保持唤醒（屏幕常亮）状态
+		 * */
+		setWakelock: function(bLock) {
+			plus.device.setWakelock(bLock);
+			if(plus.device.isWakelock()) {
+				plus.nativeUI.toast("系统应用保持唤醒(常亮)状态了");
+			} else {
+				plus.nativeUI.toast("系统应用不再保持唤醒(常亮)状态了");
+			}
+		},
+		//设置静音
+		keepSlience: function() {
+			plus.device.setVolume(0);
+			if(plus.device.getVolume() == 0) plus.nativeUI.toast("设置静音了");
+		},
+		/**
+		 * 应用程序设置屏幕亮度（只作用于程序）
+		 * @param {Number} n 亮度值（0--1），最大1，最小0
+		 * */
+		getBrightness: function(n) {
+			plus.screen.setBrightness(n);
+		},
+		/**
+		 * 锁定屏幕方向
+		 * @param {Number} n 值（0--5）或者不传
+		 * 为0，竖屏正方向；为1，竖屏反方向，屏幕正方向按顺时针旋转180°；为2，横屏正方向，屏幕正方向按顺时针旋转90°，为3，横屏方向，屏幕正方向按顺时针旋转270°；为4，竖屏正方向或反方向，根据设备重力感应器自动调整，为5，横屏正方向或反方向，根据设备重力感应器自动调整；不传值，解除锁定屏幕方向后将恢复应用默认的屏幕显示方向（通常为应用打包发布时设置的方向）。 
+		 * */
+		lockOrientation: function(n) {
+			switch(n) {
+				case 0:
+					plus.screen.lockOrientation("portrait-primary"); //竖屏正方向
+					break;
+				case 1:
+					plus.screen.lockOrientation("portrait-secondary"); //竖屏反方向，屏幕正方向按顺时针旋转180°
+					break;
+				case 2:
+					plus.screen.lockOrientation("landscape-primary"); //横屏正方向，屏幕正方向按顺时针旋转90°
+					break;
+				case 3:
+					plus.screen.lockOrientation("landscape-secondary"); //横屏方向，屏幕正方向按顺时针旋转270°
+					break;
+				case 4:
+					plus.screen.lockOrientation("portrait"); //竖屏正方向或反方向，根据设备重力感应器自动调整
+					break;
+				case 5:
+					plus.screen.lockOrientation("landscape"); // 横屏正方向或反方向，根据设备重力感应器自动调整；
+					break;
+				default:
+					plus.screen.unlockOrientation(); //解除锁定屏幕方向后将恢复应用默认的屏幕显示方向（通常为应用打包发布时设置的方向）。 
+					break;
+			}
+		},
+		/**
+		 * 拍照并保存到系统相册
+		 * @param {Function} fn 拍照成功的回调函数 返回参数为存储的路径
+		 * */
+		gallery_getImage: function(fn) {
+			var cmr = plus.camera.getCamera();
+			cmr.captureImage(function(path) {
+				plus.gallery.save(path, function() {
+					plus.nativeUI.toast('保存照片到系统相册：' + path);
+					fn(path);
+				}, function(e) {
+					plus.nativeUI.alert('保存失败: ' + JSON.stringify(e));
+				});
+			}, function(e) {
+				plus.nativeUI.toast('取消拍照');
+			}, {
+				filename: '_doc/gallery/',
+				index: 1
+			});
+		},
+		/**
+		 * 设置参数，从相册选择图片，返回回调函数
+		 * @param {Function} fn 单选相册图片成功后的回调函数，返回参数path，文件的路径(string类型)；多选则返回json格式，文件路径列表
+		 * @param {Object} options 选择相册的主要参数 
+		 * @prop {String} options.filter 可设置为仅选择图片文件（“image”）、视频文件（“video”）或所有文件（“none”），默认值为“image”。 
+		 * @prop {Boolean} options.multiple 是否多选
+		 * @prop {Number} options.maximum 最大可选的个数，仅在支持多选时有效，取值范围为1到Infinity，默认值为Infinity，即不限制选择的图片数。 如果设置的值非法则使用默认值Infinity。
+		 * @prop {Boolean} options.system 是否使用系统相册文件选择界面
+		 * */
+		gallery_pick: function(options, fn) {
+			plus.gallery.pick(
+				function(path) {
+					fn(path);
+				},
+				function(e) {
+					alert('取消选择图片');
+				}, {
+					filter: options.filter,
+					multiple: options.multiple,
+					maximum: options.maximum,
+					system: options.system,
+					onmaxed: function() {
+						plus.nativeUI.alert('最多只能选择' + options.maximum + '张图片');
+					}
+				}
+			);
+		},
+		/**
+		 * 录像并返回回调函数
+		 * @param {Function} fn 录像成功的回调函数 返回参数为存储的路径
+		 * */
+		getVideo: function(fn) {
+			plus.nativeUI.toast('开始录像...');
+			var cmr = plus.camera.getCamera();
+			cmr.startVideoCapture(function(p) {
+				plus.nativeUI.toast('录像成功：' + p);
+				fn(p);
+			}, function(e) {
+				plus.nativeUI.alert('失败：' + e.message);
+			}, {
+				filename: '_doc/camera/',
+				index: 1
+			});
+		},
+		/**
+		 * 获取定位相关信息
+		 * @param {Function} fn 回调函数 ，参数为定位的相关数据json数据格式，返回参数有坐标类型，详细地址，经纬度和时间戳等等
+		 * @param {String} sProvider 供应商地图模板，值为amap（高德地图模板），baidu（百度地图模板），system（系统地图模板）
+		 * */
+		geolocation_getInfo: function(fn, sProvider) {
+			plus.nativeUI.toast("获取定位位置信息...");
+			plus.geolocation.getCurrentPosition(
+				function(position) {
+					fn(position);
+				},
+				function(e) {
+					outSet("获取定位位置信息失败：" + e.message);
+				}, {
+					timeout: 10000,
+					geocode: true,
+					provider: sProvider, //amap>baidu>system
+					//					coordsType:'gcj02',//wgs84,bd09,gcj02,bd09ll
+				}
+			);
+		},
+		/**
+		 * 通过本地URL参数获取目录对象或文件对象(本地代码文件)
+		 * @param {String} sUrl 本地路径，比如index.html
+		 * @param {Function} fn 获取文件对象的回调函数，参数为文件对象，size为文件大小，name为文件名称，type为文件类型，lastModifiedDate为文件最后修改的时间，fullPath文件整体的路劲
+		 * @param {Function} fn2 获取文件内容的回调函数，参数为文件的内容
+		 * */
+		io_getFileSystem: function(sUrl, fn, fn2) {
+			plus.io.resolveLocalFileSystemURL("_www/" + sUrl, function(entry) {
+				// 可通过entry对象操作test.html文件 
+				entry.file(function(file) {
+					var fileReader = new plus.io.FileReader();
+					fn(file);
+					fileReader.readAsText(file, 'utf-8');
+					fileReader.onloadend = function(evt) {
+						fn2(evt.target.result);
+					}
+				});
+			}, function(e) {
+				alert("解析文件URL失败: " + e.message);
+			});
+		},
+		/**
+		 * 将本地路径转化为平台路径
+		 * @param {String} sUrl 本地url路径，如index.html
+		 * @return {String}
+		 * */
+		io_getAppUrl: function(sUrl) {
+			var path = plus.io.convertLocalFileSystemURL(sUrl);
+			return path;
+		},
+		/**
+		 * 发送短信
+		 * @param {Array} aPhoneNum 接收短信的号码组
+		 * @param {String} sContent 要发送的内容
+		 * */
+		messaging_sendSMS: function(aPhoneNum, sContent) {
+			var msg = plus.messaging.createMessage(plus.messaging.TYPE_SMS);
+			msg.to = aPhoneNum;
+			msg.body = sContent;
+			plus.messaging.sendMessage(msg);
+		},
+		/**
+		 * 发送邮件
+		 * @param {Object} option 配置参数
+		 * @prop {Array} option.to 要发送的邮箱组
+		 * @prop {Array} option.cc 要抄送的邮箱组
+		 * @prop {Array} option.to 要密送的邮箱组
+		 * @prop {String} option.title 邮件的标题(主题)
+		 * @prop {String} option.content 邮件的内容 
+		 * @prop {String} option.addAttachment 要发送邮件的附件本地URL，如_www/index.html 
+		 * */
+		messaging_sendEmail: function(option) {
+			var msg = plus.messaging.createMessage(plus.messaging.TYPE_EMAIL);
+			msg.to = option.to || []; //发送
+			msg.cc = option.cc || []; //抄送copySend
+			msg.bcc = option.bcc || []; //密送
+			msg.subject = option.title || '无标题';
+			msg.body = option.content || "";
+			if(option.addAttachment) msg.addAttachment(option.addAttachment);
+
+			plus.messaging.sendMessage(msg, function() {
+				//				plus.nativeUI.toast("发送成功!");
+			}, function() {
+				alert("发送失败!");
+			});
+		},
+		//下载对象
+		dtask: null,
+		/**
+		 * 创建下载任务
+		 * @param {String} url 下载地址		 
+		 * @param {Object} options 下载任务参数		
+		 * 
+		 * @prop {String} options.method 网络请求类型,支持http协议的“GET”、“POST”，默认为“GET”请求。
+		 * @prop {String} options.method POST请求时提交的数据 
+		 * @prop {String} options.filename 下载文件保存的路径,保存文件路径仅支持以"_downloads/"、"_doc/"、"_documents/"开头的字符串。
+		 * @prop {Number} options.priority 下载任务的优先级,数值类型，数值越大优先级越高，默认优先级值为0。 
+		 * @prop {Number} options.timeout 下载任务超时时间,数值类型，单位为s(秒)，默认值为120s。
+		 * @prop {Number} options.retry 下载任务重试次数,数值类型，默认为重试3次
+		 * @prop {Number} options.retryInterval 下载任务重试间隔时间,数值类型，单位为s(秒)，默认值为30s。 
+		 * 
+		 * @return {Object} 下载任务的对象
+		 * */
+		downloader_create: function(url, options) {
+			var opts = options || {};
+			var dtask = plus.downloader.createDownload(url, opts, function(d, status) {
+				// 下载完成
+				if(status == 200){
+					console.log(d);
+				} 
+				else{
+					plus.nativeUI.alert("下载失败，失败状态码为: " + status);
+					console.log("下载最终结果：" + JSON.stringify(d));
+				}
+			});			
+			plus.nativeUI.alert("创建下载任务成功！");
+			return dtask;
+		},
+		/**
+		 * 启动下载任务
+		 * @param {Object} dtask 下载任务对象,系创建下载任务时候返回的参数
+		 * @param {Object} obj 显示下载进度数据的DOM节点对象
+		 * @param {Function} fn 下载成功的回调函数，参数为下载文件的对象
+		 * */
+		downloader_start:function(dtask,obj,fn){
+			if(!dtask){
+				plus.nativeUI.alert("请先创建下载任务!");	
+				return;
+			}
+			dtask.start();
+			//监听下载对象的下载变更状态
+			dtask.addEventListener("statechanged", function(task, status) {				
+				switch(task.state) {
+					case 1: // 开始
+						plus.nativeUI.toast("开始下载...");
+						break;
+					case 2: // 已连接到服务器
+						plus.nativeUI.toast("链接到服务器...");
+						break;
+					case 3: // 已接收到数据						
+						obj.innerHTML = "下载数据:" + task.downloadedSize + "/" + task.totalSize ;
+						break;
+					case 4: // 下载完成
+						if(task.totalSize > 0){
+							plus.nativeUI.toast(task.filename + "下载完成！");
+							fn(task);
+						}						
+					break;
+				}
+			});
+		},
+		/**
+		 * 暂停下载任务
+		 * @param {Object} dtask 下载任务对象,系创建下载任务时候返回的参数
+		 * */
+		downloader_pause:function(dtask){
+			dtask.pause();
+			plus.nativeUI.toast("暂停下载！");
+		},
+		/**
+		 * 恢复下载任务
+		 * @param {Object} dtask 下载任务对象,系创建下载任务时候返回的参数
+		 * */
+		downloader_resume:function(dtask){
+			dtask.resume();
+			plus.nativeUI.toast("恢复下载！");
+		},
+		/**
+		 * 取消下载任务
+		 * @param {Object} dtask 下载任务对象,系创建下载任务时候返回的参数
+		 * @param {Object} obj 显示下载进度数据的DOM节点对象,与创建下载任务的节点是同一个
+		 * */
+		downloader_cancel:function(dtask,obj){
+			if(obj){obj.innerHTML = "";}			
+			dtask.abort();
+			plus.nativeUI.toast("取消下载任务了！");
+		},
 	}
 } catch(e) {
 	alert("功能方法只是在手机上有效");
